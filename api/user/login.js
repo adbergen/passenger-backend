7bfa7754d26bd6b897c51f008a8ed6b1d7ed8910
Anthony Bergen <anthonydbergen@gmail.com>
2024-05-24 16:14:27 -0400

    Added more detailed logging

--- api/user/login.js
+++ api/user/login.js
@@ -11,24 +11,34 @@ exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false
 
   try {
+    console.log('Received event:', JSON.stringify(event, null, 2))
+
     const { email, password } = JSON.parse(event.body)
+
+    console.log('Parsed payload:', { email, password })
 
     const db = await connectToDatabase()
     const collection = db.collection('users')
 
+    console.log('Connected to database.')
+
     const user = await collection.findOne({ email })
     if (!user) {
+      console.log('User not found.')
       return errorResponse('Invalid email or password')
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.passwordHash)
     if (!isPasswordValid) {
+      console.log('Invalid password.')
       return errorResponse('Invalid email or password')
     }
 
     const token = jwt.sign({ email: user.email }, jwtSecret, {
       expiresIn: '1h'
     })
+
+    console.log('Login successful.')
 
     return successResponse({ token, _id: user._id })
   } catch (error) {
